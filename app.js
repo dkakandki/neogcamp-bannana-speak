ee8eb91ee1e2f8efd493e40eb664580284ae2bfa
Swethadiv <swethak.rymec@gmail.com>
2021-10-06 14:06:23 +0530

    input output completed

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 
 var btntranslate = document.querySelector("#btn-translate");
-
 var txtinput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output");
 
-function clickeventhandler(){
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-    outputdiv.innertext="hjkhk:" + txtinput.value;
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+
+}
+
+function errorhandler(error){
+    console.log("error occured", error);
+}
+function clickeventhandler() {
+
+    var inputText = txtinput.value;
+
+fetch(getTranslationURL(inputText))
+.then(response=>response.json())
+.then(json=> {
+    var translatedtext = json.contents.translated;
+    outputdiv.innertext = translatedtext;
+})
+.catch(errorhandler)
 };
 
 btntranslate.addEventListener("click",clickeventhandler)
